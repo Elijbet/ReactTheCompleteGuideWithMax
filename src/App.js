81cabb76a94f230b95547a7797302b9116b111d5
Eliza Khachatryan <elijbet@gmail.com>
2019-04-12 12:38:22 -0700

    Creating and accessing state property in the App class.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+	state = {
+		persons: [
+			{name: 'Andy', profession: 'Aspiring Cook'},
+			{name: 'Alex', profession: 'Psychologist'},
+			{name: 'Anne', profession: 'Web Developer'}
+		]
+	}
   render() {
     return (
       <div className="App">
         <h1>Hello this is my first React setup.</h1>
-        <Person name="Andy" profession="Interior Designer"/>
-        <Person name="Alex" profession="Psychologist"/>
-        <Person name="Anne" profession="Web Developer"/>
+        <Person name="Max" profession="Interior Designer"/>
+        <Person name={this.state.persons[0].name} profession={this.state.persons[0].profession}>My hobbies: Pinterest Cooking, Instagram Exhibition.</Person>
+        <Person name={this.state.persons[1].name} profession={this.state.persons[1].profession}/>
+        <Person name={this.state.persons[2].name} profession={this.state.persons[2].profession}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-	return <p>I am { props.name }, I am {Math.floor(Math.random() * 30)} years old, and I am { props.profession }! </p>
+	return (
+		<div>
+			<p>I am { props.name }, I am {Math.floor(Math.random() * 30)} years old, and I am { props.profession }! </p>
+			<p>{ props.children }</p>
+		</div>
+		)
 	}
 
 export default person;
