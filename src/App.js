f41dbbbc7e3043ca20229f62cef7ef811394bfd7
Eliza Khachatryan <elijbet@gmail.com>
2019-04-12 11:59:50 -0700

    React first setup.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello this is my first React setup.</h1>
+        <Person />
       </div>
     );
   }
