88488557cad610addb6cd496daafb22adcedc200
Eliza Khachatryan <elijbet@gmail.com>
2019-04-12 19:30:13 -0700

    Rendering content conditionally. React is purely JS, so we don't use HTML directives to conditionally display something, but JS logic. In this case a ternary operator.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 			{name: 'Alex', profession: 'Psychologist'},
 			{name: 'Anne', profession: 'Web Developer'}
 		],
-		vampires: 'Some other form of persons.'
+		vampires: 'Some other form of persons.',
+		showPersons: false
 	}
 	switchNameHandler = (newName) => {
 		// console.log("I was clicked")
@@ -30,28 +31,45 @@ class App extends Component {
 			]
 		})
 	}
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({showPersons: !doesShow});
+	}
   render() {
+  	const style = {
+  		backgroundColor: 'white', 
+  		font: 'inherit',
+  		border: '1px solid blue',
+  		padding: '8px',
+  		cursor: 'pointer'
+  	};
     return (
       <div className="App">
         <h1>Hello this is my first React setup.</h1>
-        <Person 
-        	name="Max" 
-        	profession="Interior Designer"/>
-        <Person 
-        	name={this.state.persons[0].name} 
-        	profession={this.state.persons[0].profession}>My hobbies: Pinterest Cooking, Instagram Exhibition.
-        </Person>
-        <Person 
-        	name={this.state.persons[1].name} 
-        	profession={this.state.persons[1].profession}
-        	click={() => this.switchNameHandler('Maximilian!!')}
-        	changed={this.nameChangedHandler}/>
-        <Person 
-        	name={this.state.persons[2].name} 
-        	profession={this.state.persons[2].profession}/>
+        { 
+        	this.state.showPersons === true ?
+		        <div>
+			        <Person 
+			        	name="Max" 
+			        	profession="Interior Designer"/>
+			        <Person 
+			        	name={this.state.persons[0].name} 
+			        	profession={this.state.persons[0].profession}>My hobbies: Pinterest Cooking, Instagram Exhibition.
+			        </Person>
+			        <Person 
+			        	name={this.state.persons[1].name} 
+			        	profession={this.state.persons[1].profession}
+			        	click={() => this.switchNameHandler('Maximilian!!')}
+			        	changed={this.nameChangedHandler}/>
+			        <Person 
+			        	name={this.state.persons[2].name} 
+			        	profession={this.state.persons[2].profession}/>
+		        </div> : null
+      	}
         <button 
-        	onClick={this.switchNameHandler.bind(this, 'Max!')}>
-        	SwitchName
+        	style={style}
+        	onClick={this.togglePersonsHandler}>
+        	Toggle Persons
         </button>
       </div>
     );
